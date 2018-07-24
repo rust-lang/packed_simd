ec0574bf235d44211e364ea6556aecc4b0004e34
gnzlbg <gonzalobg88@gmail.com>
2018-07-24 12:34:30 +0200

    mandelbrot example should match benchmarks-game output by default

---
+++ examples/mandelbrot/readme.md
@@ -0,0 +1,3 @@
+# Mandelbrot benchmark from the [benchmarks game][bg].
+
+[bg]: https://benchmarksgame-team.pages.debian.net/benchmarksgame/description/mandelbrot.html#mandelbrot
--- examples/nbody/src/main.rs
+++ examples/nbody/src/main.rs
@@ -1,9 +1,15 @@
 //! The N-body benchmark from the [benchmarks game][bg].
 //!
-//! [bg]: https://benchmarksgame-team.pages.debian.net/benchmarksgame/description/nbody.html#nbody.
-//! html#nbody
+//! [bg]: https://benchmarksgame-team.pages.debian.net/benchmarksgame/description/nbody.html#nbody
 
 extern crate nbody_lib;
+
+fn run<O: ::std::io::Write>(o: &mut O, n: usize) {
+    let (energy_before, energy_after) = nbody_lib::run(n);
+
+    write!(o, "{:.9}\n", energy_before);
+    write!(o, "{:.9}\n", energy_after);
+}
 
 fn main() {
     let n: usize = std::env::args()
@@ -11,9 +17,28 @@ fn main() {
         .expect("need one arg")
         .parse()
         .expect("argument should be a usize");
+    run(&mut ::std::io::stdout(), n);
+}
 
-    let (energy_before, energy_after) = nbody_lib::run(n);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static OUTPUT: &'static [u8] = include_bytes!("nbody-output.txt");
+    #[test]
+    fn verify_output() {
+        let mut out: Vec<u8> = Vec::new();
 
-    println!("{:.9}", energy_before);
-    println!("{:.9}", energy_after);
+        run(&mut out, 1000);
+
+        assert_eq!(out.len(), OUTPUT.len());
+        if out != OUTPUT {
+            for i in 0..out.len() {
+                assert_eq!(
+                    out[i], OUTPUT[i],
+                    "byte {} differs - is: {:#08b} - should: {:#08b}",
+                    i, out[i], OUTPUT[i]
+                );
+            }
+        }
+    }
 }
