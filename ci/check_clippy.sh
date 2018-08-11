39c18592c86a586c2a9b2345d9333242912b320a
gnzlbg <gonzalobg88@gmail.com>
2018-08-11 11:13:05 +0200

    add ci/check_clippy.sh

---
+++ ci/check_clippy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Uses clippy to check the idioms of the library
+
+set -ex
+
+cargo_clippy() {
+    cargo clippy --all -- -D clippy-pedantic
+}
+
+# Check src/
+cargo_clippy
+
+# Check examples/
+for dir in examples/*/
+do
+    dir=${dir%*/}
+    cd ${dir%*/}
+    cargo_clippy
+    cd -
+done
