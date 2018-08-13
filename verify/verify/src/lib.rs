115ee40001e44c26efdaf3a3942205b2f52d3016
gnzlbg <gonzalobg88@gmail.com>
2018-08-13 14:02:58 +0200

    add abi_vectorcall to verification crate

--- verify/verify/src/lib.rs
+++ verify/verify/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(rust_2018_preview, use_extern_macros, avx512_target_feature)]
+#![feature(rust_2018_preview, use_extern_macros, avx512_target_feature, abi_vectorcall)]
 
 #[cfg(test)]
 extern crate packed_simd;
