aa2fcf3c050cbcc57eee5682dc2d6fae54fa4c8a
gnzlbg <gonzalobg88@gmail.com>
2018-09-03 16:05:15 +0200

    rename fma to mul_add

--- src/api/math/float/mul_add.rs
+++ src/api/math/float/mul_add.rs
@@ -1,11 +1,11 @@
-//! Implements vertical (lane-wise) floating-point `fma`.
+//! Implements vertical (lane-wise) floating-point `mul_add`.
 
-macro_rules! impl_math_float_fma {
+macro_rules! impl_math_float_mul_add {
     ([$elem_ty:ident; $elem_count:expr]: $id:ident | $test_tt:tt) => {
         impl $id {
             /// Fused multiply add: `self * y + z`
             #[inline]
-            pub fn fma(self, y: Self, z: Self) -> Self {
+            pub fn mul_add(self, y: Self, z: Self) -> Self {
                 use crate::codegen::math::float::fma::Fma;
                 Fma::fma(self, y, z)
             }
@@ -14,28 +14,28 @@ macro_rules! impl_math_float_fma {
         test_if!{
             $test_tt:
             interpolate_idents! {
-                pub mod [$id _math_fma] {
+                pub mod [$id _math_mul_add] {
                     use super::*;
                     #[cfg_attr(not(target_arch = "wasm32"), test)] #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
-                    fn fma() {
+                    fn mul_add() {
                         let z = $id::splat(0 as $elem_ty);
                         let o = $id::splat(1 as $elem_ty);
                         let t = $id::splat(2 as $elem_ty);
                         let t3 = $id::splat(3 as $elem_ty);
                         let f = $id::splat(4 as $elem_ty);
 
-                        assert_eq!(z, z.fma(z, z));
-                        assert_eq!(o, o.fma(o, z));
-                        assert_eq!(o, o.fma(z, o));
-                        assert_eq!(o, z.fma(o, o));
+                        assert_eq!(z, z.mul_add(z, z));
+                        assert_eq!(o, o.mul_add(o, z));
+                        assert_eq!(o, o.mul_add(z, o));
+                        assert_eq!(o, z.mul_add(o, o));
 
-                        assert_eq!(t, o.fma(o, o));
-                        assert_eq!(t, o.fma(t, z));
-                        assert_eq!(t, t.fma(o, z));
+                        assert_eq!(t, o.mul_add(o, o));
+                        assert_eq!(t, o.mul_add(t, z));
+                        assert_eq!(t, t.mul_add(o, z));
 
-                        assert_eq!(f, t.fma(t, z));
-                        assert_eq!(f, t.fma(o, t));
-                        assert_eq!(t3, t.fma(o, o));
+                        assert_eq!(f, t.mul_add(t, z));
+                        assert_eq!(f, t.mul_add(o, t));
+                        assert_eq!(t3, t.mul_add(o, o));
                     }
                 }
             }
--- src/api/math/float/mul_adde.rs
+++ src/api/math/float/mul_adde.rs
@@ -1,15 +1,15 @@
-//! Implements vertical (lane-wise) floating-point `fmae`.
+//! Implements vertical (lane-wise) floating-point `mul_adde`.
 
-macro_rules! impl_math_float_fmae {
+macro_rules! impl_math_float_mul_adde {
     ([$elem_ty:ident; $elem_count:expr]: $id:ident | $test_tt:tt) => {
         impl $id {
             /// Fused multiply add estimate: ~= `self * y + z`
             ///
             /// While fused multiply-add (`fma`) has infinite precision,
-            /// `fmae` has at worst the same precision of a multiply followed by an add.
+            /// `mul_adde` has at worst the same precision of a multiply followed by an add.
             /// This might be more efficient on architectures that do not have an `fma` instruction.
             #[inline]
-            pub fn fmae(self, y: Self, z: Self) -> Self {
+            pub fn mul_adde(self, y: Self, z: Self) -> Self {
                 use crate::codegen::math::float::fmae::Fmae;
                 Fmae::fmae(self, y, z)
             }
@@ -18,28 +18,28 @@ macro_rules! impl_math_float_fmae {
         test_if!{
             $test_tt:
             interpolate_idents! {
-                pub mod [$id _math_fmae] {
+                pub mod [$id _math_mul_adde] {
                     use super::*;
                     #[cfg_attr(not(target_arch = "wasm32"), test)] #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
-                    fn fmae() {
+                    fn mul_adde() {
                         let z = $id::splat(0 as $elem_ty);
                         let o = $id::splat(1 as $elem_ty);
                         let t = $id::splat(2 as $elem_ty);
                         let t3 = $id::splat(3 as $elem_ty);
                         let f = $id::splat(4 as $elem_ty);
 
-                        assert_eq!(z, z.fmae(z, z));
-                        assert_eq!(o, o.fmae(o, z));
-                        assert_eq!(o, o.fmae(z, o));
-                        assert_eq!(o, z.fmae(o, o));
+                        assert_eq!(z, z.mul_adde(z, z));
+                        assert_eq!(o, o.mul_adde(o, z));
+                        assert_eq!(o, o.mul_adde(z, o));
+                        assert_eq!(o, z.mul_adde(o, o));
 
-                        assert_eq!(t, o.fmae(o, o));
-                        assert_eq!(t, o.fmae(t, z));
-                        assert_eq!(t, t.fmae(o, z));
+                        assert_eq!(t, o.mul_adde(o, o));
+                        assert_eq!(t, o.mul_adde(t, z));
+                        assert_eq!(t, t.mul_adde(o, z));
 
-                        assert_eq!(f, t.fmae(t, z));
-                        assert_eq!(f, t.fmae(o, t));
-                        assert_eq!(t3, t.fmae(o, o));
+                        assert_eq!(f, t.mul_adde(t, z));
+                        assert_eq!(f, t.mul_adde(o, t));
+                        assert_eq!(t3, t.mul_adde(o, o));
                     }
                 }
             }
