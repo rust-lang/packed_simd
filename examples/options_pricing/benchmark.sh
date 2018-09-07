8773103b2c21cc753d026838c1ea00ef3fd4ed11
gnzlbg <gonzalobg88@gmail.com>
2018-09-07 13:42:18 +0200

    add parallel options pricing implementation

--- examples/options_pricing/benchmark.sh
+++ examples/options_pricing/benchmark.sh
@@ -4,17 +4,19 @@
 
 set -ex
 
-NUM_OPTIONS=10000000
+NUM_OPTIONS_BLACK_SCHOLES=10000000
 
 if [[ ${NORUN} != 1 ]]; then
     hash hyperfine 2>/dev/null || { echo >&2 "hyperfine is not in PATH."; exit 1; }
 fi
 
-ALGS=("black_scholes_scalar" "black_scholes_simd" "binomial_put_scalar" "binomial_put_simd")
+# Black-Scholes:
+ALGS=("black_scholes_scalar" "black_scholes_simd" "black_scholes_simd_par")
 if echo "$FEATURES" | grep -q "ispc"; then
     hash ispc 2>/dev/null || { echo >&2 "ispc is not in PATH."; exit 1; }
-    ALGS+=("black_scholes_ispc" "black_scholes_ispc_tasks" "binomial_put_ispc" "binomial_put_ispc_tasks")
+    ALGS+=("black_scholes_ispc" "black_scholes_ispc_tasks")
 fi
+
 
 RUSTFLAGS="-C target-cpu=native ${RUSTFLAGS}" \
          cargo build --release --features="${FEATURES}"
@@ -25,5 +27,18 @@ fi
 
 for alg in "${ALGS[@]}"
 do
-    hyperfine "target/release/options_pricing ${NUM_OPTIONS} ${alg}"
+    hyperfine "target/release/options_pricing ${NUM_OPTIONS_BLACK_SCHOLES} ${alg}"
+done
+
+# Binomial put:
+ALGS=("binomial_put_scalar" "binomial_put_simd" "binomial_put_simd_par")
+if echo "$FEATURES" | grep -q "ispc"; then
+    ALGS+=("binomial_put_ispc" "binomial_put_ispc_tasks")
+fi
+
+NUM_OPTIONS_BINOMIAL_PUT=500000
+
+for alg in "${ALGS[@]}"
+do
+    hyperfine "target/release/options_pricing ${NUM_OPTIONS_BINOMIAL_PUT} ${alg}"
 done
