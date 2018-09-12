c0b902e66cbb692c6a8c5b675b60c347845afb7a
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-09-12 08:13:16 +0300

    Disable running examples on sparc64
    It times out too often.

--- ci/run_examples.sh
+++ ci/run_examples.sh
@@ -8,7 +8,8 @@ fi
 
 # FIXME: travis exceeds 50 minutes on these targets
 # Skipping the examples is an attempt at preventing travis from timing-out
-if [[ ${TARGET} == "arm-linux-androidabi" ]] || [[ ${TARGET} == "aarch64-linux-androidabi" ]]; then
+if [[ ${TARGET} == "arm-linux-androidabi" ]] || [[ ${TARGET} == "aarch64-linux-androidabi" ]] \
+    || [[ ${TARGET} == "sparc64-unknown-linux-gnu" ]]; then
     exit 0
 fi
 
