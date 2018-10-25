77d2bb60666db4d528e9bde85e2eb81cf7c9202b
gnzlbg <gonzalobg88@gmail.com>
2018-10-25 14:27:18 +0200

    add more workarounds for the rustc unused_mut bug

--- src/api/ops/scalar_shifts.rs
+++ src/api/ops/scalar_shifts.rs
@@ -36,7 +36,8 @@ macro_rules! impl_ops_scalar_shifts {
                     use super::*;
                     #[cfg_attr(not(target_arch = "wasm32"), test)] #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
                     #[cfg_attr(any(target_arch = "s390x", target_arch = "sparc64"),
-                               allow(unreachable_code, unused_variables))]
+                               allow(unreachable_code, unused_variables, unused_mut))]
+                    // ^^^ FIXME: https://github.com/rust-lang/rust/issues/55344
                     fn ops_scalar_shifts() {
                         let z = $id::splat(0 as $elem_ty);
                         let o = $id::splat(1 as $elem_ty);
