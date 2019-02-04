fa6f16208b32ca1eb11ed34195557802398bdeca
Niv Kaminer <nivkner@zoho.com>
2019-02-04 02:20:46 +0200

    use chunks instead of slicing with indexes

--- examples/dot_product/src/simd.rs
+++ examples/dot_product/src/simd.rs
@@ -6,14 +6,12 @@ pub fn dot_prod(a: &[f32], b: &[f32]) -> f32 {
     assert_eq!(a.len(), b.len());
     assert!(a.len() % 4 == 0);
 
-    let mut sum = f32x4::splat(0.);
-
-    for i in (0..a.len()).step_by(4) {
-        sum += f32x4::from_slice_unaligned(&a[i..])
-            * f32x4::from_slice_unaligned(&b[i..]);
-    }
-
-    sum.sum()
+    a.chunks_exact(4)
+        .map(f32x4::from_slice_unaligned)
+        .zip(b.chunks_exact(4).map(f32x4::from_slice_unaligned))
+        .map(|(a, b)| a * b)
+        .sum::<f32x4>()
+        .sum()
 }
 
 #[cfg(test)]
