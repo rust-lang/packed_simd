f421eefd3444ef1d314e9af5b052e05fb3bdebd9
Jubilee <46493976+workingjubilee@users.noreply.github.com>
2020-09-26 14:27:55 -0700

    Rebuild CI stages (#295)
    Rebuilds CI into a set of stages that indicates first the "totally
    supported" stage, marked "build-test-verify", then groups arches
    by base pointer width and tier. This surfaces all targets which
    currently have full expected functionality, so as to better identify
    and limit future regressions.
    
    - Allow improper ctypes in project to quash spurious CI error
    - Remove Intel SDE, as the Dockerfile is broken.
    - Use recent LTS Ubuntu in CI
    - Fix deprecation warnings for .travis.yml
    - Allow inlining failure on 32-bit platforms
    - Always test aarch64 with Neon
    - Move promising platforms to build-test-verify
    - Drop tier 3 targets
    - Reduce repetitive SSE2 -> SSE4 tests
    - Allow aarch64-apple-ios + Neon test failure

--- verify/verify/src/lib.rs
+++ verify/verify/src/lib.rs
@@ -1,3 +1,6 @@
+// FIXME: these types are unsound in C FFI already
+// See https://github.com/rust-lang/rust/issues/53346
+#![allow(improper_ctypes_definitions)]
 #![deny(warnings, rust_2018_idioms)]
 #![cfg_attr(test, feature(avx512_target_feature, abi_vectorcall, llvm_asm))]
 
