728d47506fe3a2599818060bd6e02e4bd3e338d5
Jubilee Young <workingjubilee@gmail.com>
2021-05-15 20:15:42 -0700

    Update shuffle API for const generics

--- src/lib.rs
+++ src/lib.rs
@@ -212,6 +212,7 @@
 //! guide](https://rust-lang-nursery.github.io/packed_simd/perf-guide/)
 
 #![feature(
+    const_generics,
     repr_simd,
     rustc_attrs,
     platform_intrinsics,
@@ -229,6 +230,7 @@
         // FIXME: these types are unsound in C FFI already
         // See https://github.com/rust-lang/rust/issues/53346
         improper_ctypes_definitions,
+        incomplete_features,
         clippy::cast_possible_truncation,
         clippy::cast_lossless,
         clippy::cast_possible_wrap,
@@ -239,6 +241,7 @@
         // See https://github.com/rust-lang/rust-clippy/issues/3410
         clippy::use_self,
         clippy::wrong_self_convention,
+        clippy::from_over_into,
 )]
 #![cfg_attr(test, feature(hashmap_internals))]
 #![deny(rust_2018_idioms, clippy::missing_inline_in_public_items)]
